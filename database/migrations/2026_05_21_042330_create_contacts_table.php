5b87b668986c0f21509017c842e89f65173e0d4a
kotone <osakaaaana-osigoto@yahoo.co.jp>
2026-05-21 14:22:55 +0900

    chore: run pint and format code

--- database/migrations/2026_05_21_042330_create_contacts_table.php
+++ database/migrations/2026_05_21_042330_create_contacts_table.php
@@ -14,14 +14,14 @@ return new class extends Migration
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id')->constrained()->cascadeOnDelete();
-            $table->string('first_name',255);
-            $table->string('last_name',255);
+            $table->string('first_name', 255);
+            $table->string('last_name', 255);
             $table->tinyInteger('gender');
-            $table->string('email',255);
-            $table->string('tel',11);
-            $table->string('address',255);
-            $table->string('building',255)->nullable();
-            $table->string('detail',120);
+            $table->string('email', 255);
+            $table->string('tel', 11);
+            $table->string('address', 255);
+            $table->string('building', 255)->nullable();
+            $table->string('detail', 120);
             $table->timestamps();
         });
     }
--- database/migrations/2026_05_21_042412_create_contact_tag_table.php
+++ database/migrations/2026_05_21_042412_create_contact_tag_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('contact_tag', function (Blueprint $table) {
             $table->id();
-            
+
             $table->foreignId('contact_id')
                 ->constrained('contacts')
                 ->cascadeOnDelete();
