78499318aed4503a834453fa51b479f296168347
kotone <osakaaaana-osigoto@yahoo.co.jp>
2026-05-22 09:07:10 +0900

    feat: モデル作成とリレーション定義 #2

---
+++ app/Models/Contact.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use App\Models\Category;
+use App\Models\Tag;
+
+class Contact extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'first_name',
+        'last_name',
+        'gender',
+        'email',
+        'tel',
+        'address',
+        'building',
+        'detail',
+    ];
+
+    public function getGenderLabelAttribute(): string
+    {
+        return match ($this->gender) {
+            1 => '男性',
+            2 => '女性',
+            3 => 'その他',
+        };
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, 'contact_tag');
+    }
+}
